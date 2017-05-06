917831034253624e9e5d34f5ea0783f09cae84ab
root <root@localhost.localdomain>
2017-05-06 11:05:44 +0000

    added dependency injection to TestController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Hoge;
+use DB;
 
 class TestController extends Controller
 {
@@ -13,8 +14,28 @@ class TestController extends Controller
         // 「そういう気分じゃないんだ...」だそう。
         // つまりインスタンスが新しく生成されていまっている。
         $hoge->sayHello();
+	$users = DB::select('select * from posts');
+	
+	$result = DB::table('tickets')
+            ->where('title', '=', 'John')
+            ->orWhere(function ($query) {
+                $query->where('posts_id', '>', 1)
+                      ->where('status', '<>', 'open');
+            })
+            ->get();
 
+	//DB::table('posts')->remember(120)->get();
+       
+	//$column_diff = $users = DB::table('tickets')
+        //        ->whereColumn('posts_id', 'task_id');
+ 
+	//return view('about', ['users' => $users , 'column' => $column_diff]); 
+	//return view('about', ['users' => $users]); 
+	return view('about', ['users' => $result]); 
+        //$user = $this->users->find($id);
+
+        //return view('user.profile', ['user' => $user]);
         // 「こんにちは！」
-        //\App::make('hoge')->sayHello();
+       //\App::make('hoge')->sayHello();
     }
 }
