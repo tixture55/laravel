cf2a84822317da26fcb30e8279a2372f19e0ddf9
root <root@localhost.localdomain>
2017-05-07 09:38:21 +0000

    modified blade of view being set

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,16 +3,41 @@
 if(isset($users)){
 	print_r($users);
 }
+
 ?>
+
 <html>
 <head>
+    <!-- cssの呼び出し -->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/app.css" rel="stylesheet" type="text/css">
     <title>About</title>
 </head>
 <body>
     <h1>About Me</h1>
-
+<table border="1">
+<tbody>
+        <tr>
+	    <td>便名</td>
+	    <td>航空会社：国籍</td>
+	    <td>航空会社名</td>
+	    <td>便発起日時</td>
+	</tr>
+    @foreach($users as $member)
+	<tr>
+            <td>{{ $member->name }}</td>
+            <td>{{ $member->national_flg}}</td>
+            <td>{{ $member->airline }}</td>
+            <td>{{ $member->created_at }}</td>
+        </tr>
+    @endforeach
+</tbody>
+</table>
+<br>
+<br>
+<br>
 <form action="/about" method="post">
-  <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+  {{ csrf_field() }}
   <div>
     全角カタカナ
     <input type="text" pattern="[\u30A1-\u30FF]*"/>
